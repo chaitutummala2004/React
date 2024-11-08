e5f59b0468b9cd83dbea69abf441adb5c19cfcc5
Sai Chaitanya Tummala <221fa04668@gmail.com>
2024-11-08 23:55:19 +0530

    Update Register.jsx

--- interface/src/pages/Register.jsx
+++ interface/src/pages/Register.jsx
@@ -144,7 +144,7 @@ const Register = () => {
       try {
         setLoading(true); // Show loading spinner
         const response = await axios.post(
-          "http://localhost:5004/user/register",
+          "https://vercel.com/sruthi-bajjuris-projects/react-k2ju",
           formData
         );
         setSuccessMessage(response.data.message);
